8f3f7d8e5dc78a7a8fe5c148498f8973ec80d046
khalidzahra <khalidzahra923@gmail.com>
2023-02-19 11:52:27 +0200

    Add logout and user fetching logic

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -1,8 +1,54 @@
 import React, { Component } from 'react';
 import Head from 'next/head'
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-const Layout = ({children}) => {
+const Layout = (props) => {
+
+    const router = useRouter();
+
+    const logout = async () => {
+        await fetch('http://localhost:8000/api/auth/logout', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            credentials: 'include'
+        });
+        await router.push('/login');
+    };
+
+    let navLinks;
+
+    if (!props.auth) {
+        navLinks = (
+            <div>
+                <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                    <li className="nav-item">
+                        <Link className="nav-link" href={"/login"}>
+                            Login
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" href={"/register"}>
+                            Register
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        );
+    } else {
+        navLinks = (
+            <div>
+                <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                    <li className="nav-item">
+                        <Link className="nav-link" href={""} onClick={logout}>
+                            Logout
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+
     return (
         <>
         <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
@@ -10,25 +56,12 @@ const Layout = ({children}) => {
                 <Link className="navbar-brand" href={"/"}>
                     Home
                 </Link>
-                <div>
-                    <ul className="navbar-nav me-auto mb-2 mb-md-0">
-                        <li className="nav-item">
-                            <Link className="nav-link" href={"/login"}>
-                                Login
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" href={"/register"}>
-                                Register
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+                {navLinks}
             </div>
         </nav>
 
         <main className="form-signin w-100 m-auto">
-            {children}
+            {props.children}
         </main>
       </>
     );
