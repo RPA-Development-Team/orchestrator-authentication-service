a98a0b94f9316bef7ae49af731e6710c2cb60d41
khalidzahra <khalidzahra923@gmail.com>
2023-02-19 10:50:22 +0200

    Add template pages

---
+++ pages/register.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Layout from '@/layouts/Layout';
+
+const Register = () => {
+    return (
+        <Layout>
+            Register
+        </Layout>
+    );
+};
+
+export default Register;
