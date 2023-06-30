58344bfe949e4c9961ad138f9666c79b8cfbad61
khalidzahra <khalidzahra923@gmail.com>
2023-07-01 01:53:38 +0300

    Change auth route to authenticate to avoid conflicts with NextAuth

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authRoutes = require('./authRoutes');
 const licenseRoutes = require('./licenseRoutes');
 
-router.use('/auth', authRoutes);
+router.use('/authenticate', authRoutes);
 router.use('/license', licenseRoutes);
 
 module.exports = router;
