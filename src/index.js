647a68070141921935ff6c0b2d946ed4970b5ad9
khalidzahra <khalidzahra923@gmail.com>
2023-07-06 02:53:53 +0300

    Fix cors error

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use('/api', routes);
-
 app.use(cors({
-    origin: '*'
-  }));
+  origin: '*'
+}));
+
+app.use('/api', routes);
   
 
 keycloakConsumer.consume();
