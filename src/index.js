c19267363157202afeb323c7e41f5fa44e2cc68b
khalidzahra <khalidzahra923@gmail.com>
2023-07-06 02:15:42 +0300

    Handle cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const keycloakConsumer = require('./keycloak/keycloak-consumer');
 const routes = require('./routes/routes');
 const app = express();
@@ -6,6 +7,11 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use('/api', routes);
+
+app.use(cors({
+    origin: '*'
+  }));
+  
 
 keycloakConsumer.consume();
 
