a66d5bb2a3a589a74327405784ed67d1f7f39dde
khalidzahra <khalidzahra923@gmail.com>
2023-07-05 07:45:05 +0300

    Make license endpoints use uuids

--- src/controllers/LicenseController.js
+++ src/controllers/LicenseController.js
@@ -2,16 +2,36 @@ const { prisma }  = require('rpa-prisma-module');
 
 exports.getUserLicense = async (req, res, next) => {
     try {
-        const userLicense = await prisma.userLicense.findFirst({
+        const user = await prisma.userAccount.findUnique({
             where: {
-                adminID: req.decodedUser.id
+                uuid: req.decodedUser.uuid
             },
             include: {
-                license: true
+                userLicense: {
+                    include: {
+                        license: true
+                    }
+                }
             }
         });
-    
-        res.send(userLicense.license);
+
+        if (user.userType == "ADMIN") {
+            res.send(user.userLicense.license);
+        } else {
+            let admin = await prisma.userAccount.findUnique({
+                where: {
+                    id: user.adminID
+                },
+                include: {
+                    userLicense: {
+                        include: {
+                            license: true
+                        }
+                    }
+                }
+            });
+            res.send(admin.userLicense.license);
+        }
     } catch (err) {
         res.status(404).send({
             message: "User not found."
@@ -20,12 +40,31 @@ exports.getUserLicense = async (req, res, next) => {
 }
 
 exports.setUserLicense = async (req, res, next) => {
-    const { userId, licenseId } = req.body;
+    const { licenseId } = req.body;
     
     try {
+        const user = await prisma.userAccount.findUnique({
+            where: {
+                uuid: req.decodedUser.uuid
+            },
+            include: {
+                userLicense: {
+                    include: {
+                        license: true
+                    }
+                }
+            }
+        });
+
+        if (user.userType != "ADMIN") {
+            return res.status(403).json({
+                message: "User is not an Admin."
+            })
+        }
+
         const userLicense = await prisma.userLicense.update({
             where: {
-                userID: userId
+                userID: user.id
             },
             data: {
                 license: {
